6d58d698240cfcb3984aef542d3221d3e8d39db9
Gabriel Reis <gabsrs@gitbasic.com>
2025-01-30 04:33:59 -0300

    feat: tz identifier/utc offset route

--- src/api.py
+++ src/api.py
@@ -1,6 +1,8 @@
 from flask_restx import Api
 from .routes.date_route import api as date
+from .routes.timezones import api as timezones
 
 api = Api(bundle_errors=True)
 
 api.add_namespace(date, path='/date')
+api.add_namespace(timezones, path='/timezones')
