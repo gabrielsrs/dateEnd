da8787ee674b92bd21d9e0a563f6540f8c0de3d1
Gabriel Reis <gabsrs@gitbasic.com>
2025-02-15 20:49:29 -0300

    feat: serialize data from request parsing error

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 from flask_restx import Api
 from werkzeug.exceptions import HTTPException
+from collections import OrderedDict
 
 from .routes.date_route import api as date
 from .routes.timezones_route import api as timezones
@@ -8,6 +9,19 @@ api = Api(bundle_errors=True)
 
 @api.errorhandler(HTTPException)
 def http_exception_error_handler(error):
+    if hasattr(error, "data"):
+        error.data = OrderedDict([
+            ("code", error.code),
+            ("message", error.data["message"] or "An unexpected error occurred"),
+            ("errors", error.data["errors"]),
+        ])
+
+        return {
+            "code": error.code,
+            "message": error.data["message"] or "An unexpected error occurred",
+            "error": error.data["errors"],
+        }, error.code
+
     return {
         "code": error.code,
         "message": error.description or "An unexpected error occurred",
