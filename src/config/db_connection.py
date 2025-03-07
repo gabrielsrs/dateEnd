9fe7f29c986f53cb22ffb4c4d5452a29b16ef3bd
Gabriel Reis <gabsrs@gitbasic.com>
2025-03-07 20:16:13 -0300

    docs: config docstring

--- src/config/db_connection.py
+++ src/config/db_connection.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 def create_conn():
+    """Create database connection"""
     client = MongoClient(os.getenv("MONGO_CONNECTION"))
     db = client.dateEnd
     collection = db.dates
