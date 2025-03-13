a731dda128efd66003548581f747644d6f0a02a4
Gabriel Reis <gabsrs@gitbasic.com>
2025-03-12 22:36:30 -0300

    docs: validate_parsers docstrings

--- src/utils/validation_parsers/id_validator.py
+++ src/utils/validation_parsers/id_validator.py
@@ -3,12 +3,27 @@ import re
 from werkzeug.exceptions import BadRequest
 
 def id_validator(assign):
+    """
+    A decorator to validate id
+
+    :return: The wrapper function
+    """
     DATE_REGEX = re.compile(r"^[a-fA-F0-9]{24}$")
 
     @wraps(assign)
     def wrapper(*args, **kwargs):
+        """
+        Validate date id format
+
+        :param: Expected a date_id in kwargs
+
+        :raise BadRequest: If date_id not match with regex
+
+        :return: All args and kwargs
+        """
         date_id = kwargs.get('date_id')
         if not DATE_REGEX.match(date_id):
             raise BadRequest("Invalid date Id")
         return assign(*args, **kwargs)
+    
     return wrapper
